3f474a4c51cc4e1475739b9fd4f719ccdec16f59
Ben <bobrien@ideaventionsacademy.org>
2017-11-21 11:20:18 -0500

    This is a password and username checker.

--- c1_6_password_checker.py
+++ c1_6_password_checker.py
@@ -1,5 +1,22 @@
-# Name
-# Date
+# Alexandra Fuhs
+# 11/15/17
 # Python Beginnings
 # 6 Password Checker
+
+import time
+username = "Pusheen"
+password = "tuRk3y"
+print("starting up...")
+time.sleep(3)
+
+name = input("What is your username?")
+pw = input ("What is the password?")
+
+if name == username:
+    if pw == password:
+        print ("Access granted!")
+    else:
+        print ("Incorrect!")
+else:
+    print ("Incorrect!")
 
